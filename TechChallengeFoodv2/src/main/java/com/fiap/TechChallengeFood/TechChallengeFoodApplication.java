20bc40efc75635d12adc4f045cfb207b2d061493
grupo27-6soat-fiap <167378602+grupo27-6soat-fiap@users.noreply.github.com>
2024-05-13 21:11:12 -0300

    TechChallengeFoodv2

---
+++ TechChallengeFoodv2/src/main/java/com/fiap/TechChallengeFood/TechChallengeFoodApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.TechChallengeFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFoodApplication.class, args);
+	}
+
+}
---
+++ TechChallengeFoodv2/src/test/java/com/fiap/TechChallengeFood/TechChallengeFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.TechChallengeFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
