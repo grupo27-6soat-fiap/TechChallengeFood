cb0bbed688144ff808403969526800905f078798
grupo27-6soat-fiap <grupo27.6soat.fiap@gmail.com>
2024-05-14 22:30:54 -0300

    Ajustes

--- java-mysql/src/main/java/com/example/accessingdatamysql/ClienteRepository.java
+++ java-mysql/src/main/java/com/example/accessingdatamysql/ClienteRepository.java
@@ -2,11 +2,11 @@ package com.example.accessingdatamysql;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.accessingdatamysql.User;
+import com.example.accessingdatamysql.Cliente;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 
 }
