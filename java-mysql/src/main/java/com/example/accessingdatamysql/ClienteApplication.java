46dc49d83d57edab94e0e8334b0590005edd358c
grupo27-6soat-fiap <grupo27.6soat.fiap@gmail.com>
2024-05-14 22:39:08 -0300

    Refatoracao

--- java-mysql/src/main/java/com/example/accessingdatamysql/ClienteApplication.java
+++ java-mysql/src/main/java/com/example/accessingdatamysql/ClienteApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataMysqlApplication {
+public class ClienteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+		SpringApplication.run(ClienteApplication.class, args);
 	}
 }
