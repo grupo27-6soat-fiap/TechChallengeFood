b0eff549f2b355e7d75dea52ace1250c4e8cc8dd
grupo27-6soat-fiap <grupo27.6soat.fiap@gmail.com>
2024-05-16 21:30:05 -0300

    FR - Incluido metodos de Consulta de Cliente

--- food-techchallenge-api/src/main/java/food/techchallenge/api/controller/ClienteController.java
+++ food-techchallenge-api/src/main/java/food/techchallenge/api/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package food.techchallenge.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import food.techchallenge.api.domain.cliente.entity.Cliente;
@@ -23,5 +26,17 @@ public class ClienteController {
        
         clienteRepository.save(new Cliente(dados));
     }
+
+   @GetMapping("/obterTodosCliente")
+    public @ResponseBody Iterable<Cliente> getAllClientes() {
+       
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/obterCliente")
+    public @ResponseBody Iterable<Cliente> getCliente(@RequestParam String cpf) {
+        return clienteRepository.findByCpf(cpf);
+    }
+
 
 }
--- food-techchallenge-api/src/main/java/food/techchallenge/api/domain/cliente/interfaces/repository/IClienteRepository.java
+++ food-techchallenge-api/src/main/java/food/techchallenge/api/domain/cliente/interfaces/repository/IClienteRepository.java
@@ -1,10 +1,11 @@
 package food.techchallenge.api.domain.cliente.interfaces.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import food.techchallenge.api.domain.cliente.entity.Cliente;
-
 @Repository
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
-
+    List<Cliente> findByCpf(String cpf);
 }
