47f8f62f7361c4d395c1ee4fc1fd4b2948185ec5
grupo27-6soat-fiap <grupo27.6soat.fiap@gmail.com>
2024-09-01 21:23:08 -0300

    Estrutura Fase 3

--- food-techchallenge-api/src/main/java/food/techchallenge/api/infrastructure/controlers/produto/CreateProdutoResponse.java
+++
@@ -1,5 +0,0 @@
-package food.techchallenge.api.infrastructure.controlers.produto;
-
-public record CreateProdutoResponse(Long id, String nome, String categoria, String descricao, Double preco) {
-
-}
--- food-techchallenge-api/src/main/java/food/techchallenge/api/infrastructure/persistence/PedidoProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package food.techchallenge.api.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoProdutoRepository extends JpaRepository<ProdutoPedidoEntity, Long> {
-
-}
